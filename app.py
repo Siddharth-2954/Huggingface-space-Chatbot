739ca44ba05a3926584f6f1bfbc40059e9e1c0a2
Siddharth <bharadiasj@gmail.com>
2026-08-02 19:18:11 +0530

    Added

--- app.py
+++ app.py
@@ -5,120 +5,131 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
-from langchain.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+from langchain.chains import (create_retrieval_chain, create_history_aware_retriever)
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from transformers import AutoTokenizer
 
 load_dotenv()
 
-st.set_page_config(page_title="Conversation RAG with PDF + Message History GROQ")
-
 st.title("Conversation RAG with PDF + Message History GROQ")
 
-api_key = st.text_input("Enter your GROQ API Key", type="password")
+api_key = st.text_input("Provide with the GROQ API Key", type='password')
 
 if api_key:
+    model = ChatGroq(model='llama-3.1-8b-instant', groq_api_key=api_key)
 
-    llm = ChatGroq(
-        model="llama-3.1-8b-instant",
-        groq_api_key=api_key
-    )
+    session_id = st.text_input("Please Provide Your Session ID", value='default-session')
 
-    uploaded_file = st.file_uploader(
-        "Upload a PDF",
-        type="pdf"
-    )
+    if 'store' not in st.session_state:
+        st.session_state.store={}
+        
+    uploaded_file = st.file_uploader("Upload Your PDFs: ", type='pdf')
 
     if uploaded_file:
-
-        with open("temp.pdf", "wb") as f:
-            f.write(uploaded_file.getbuffer())
-
-        st.info("Loading PDF...")
-
-        loader = PyPDFLoader("temp.pdf")
+        temp_pdf="./temporary.pdf"
+        with open(temp_pdf, 'wb') as f:
+            f.write(uploaded_file.getvalue())
+        
+        # Load PDF
+        loader = PyPDFLoader(temp_pdf)
         documents = loader.load()
+        
+        # Debug: checking if PDF loaded correctly 
+        print(f"PDF loaded with {len(documents)} pages")
+        
+        # Split text into chunks
+        splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
+        
+        splits = splitter.split_documents(documents)
+        vector = Chroma.from_documents(splits, embedding=HuggingFaceEmbeddings(), persist_directory="./chroma_datab")
+        retriever = vector.as_retriever()
 
-        st.success(f"Loaded {len(documents)} pages")
+        # Code for storing history
 
-        splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1000,
-            chunk_overlap=200
-        )
+        contextualize_system_prompt= '''Given a chat history and the latest user question, reforumlate the question to make it standalone. Do not answer only rephrase.'''
 
-        chunks = splitter.split_documents(documents)
+        contextualize_prompt = ChatPromptTemplate.from_messages([('system', contextualize_system_prompt), MessagesPlaceholder('chat_history'), ('human', '{input}')])
 
-        st.success(f"Created {len(chunks)} text chunks")
+        history_aware_retriever = create_history_aware_retriever(model, retriever, contextualize_prompt)
 
-        st.info("Loading Embedding Model...")
+        system_prompt = '''You are an helpful AI assistant. For Question Answer from the provided  context only. If unsure, Say I don't know context 
+        {context}
+        '''
 
-        embeddings = HuggingFaceEmbeddings(
-            model_name="sentence-transformers/all-MiniLM-L6-v2"
-        )
+        qa_prompt = ChatPromptTemplate.from_messages([('system', system_prompt), MessagesPlaceholder('chat_history'), ('human', '{input}')])
 
-        st.success("Embeddings Loaded")
+        document_chain = create_stuff_documents_chain(model, qa_prompt)
 
-        st.info("Creating Vector Database...")
+        retrieval_chain = create_retrieval_chain(history_aware_retriever, document_chain)
 
-        vectorstore = Chroma.from_documents(
-            documents=chunks,
-            embedding=embeddings
-        )
+        
+        tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
+        
+        MAX_TOKENS = 1200
 
-        retriever = vectorstore.as_retriever()
+        MAX_MESSAGES = 6
 
-        st.success("Vector Database Created")
+        def trim_history_by_tokens(history):
 
-        prompt = ChatPromptTemplate.from_template(
-            """
-You are a helpful AI assistant.
+            total_tokens = 0
+            trimmed_history = []
 
-Answer the question only from the provided context.
+            for message in reversed(history.messages):
+                tokens = len(tokenizer.encode(message.content))
+                if total_tokens + tokens > MAX_TOKENS:
+                    break
 
-If the answer is not available in the context, say:
+                total_tokens += tokens
+                trimmed_history.insert(0, message)
 
-"I don't know based on the uploaded PDF."
+            history.messages = trimmed_history
 
-Context:
-{context}
+            return total_tokens
 
-Question:
-{input}
-"""
-        )
 
-        document_chain = create_stuff_documents_chain(
-            llm,
-            prompt
-        )
+        def get_session_history(session_id):
 
-        retrieval_chain = create_retrieval_chain(
-            retriever,
-            document_chain
-        )
+            if session_id not in st.session_state.store:
+                st.session_state.store[session_id] = ChatMessageHistory()
 
-        question = st.text_input("Ask a question from the PDF")
+            history = st.session_state.store[session_id]
 
-        if question:
+            total_tokens = trim_history_by_tokens(history)
 
-            with st.spinner("Generating Answer..."):
+            st.sidebar.write(f"Current Tokens : {total_tokens}")
 
-                response = retrieval_chain.invoke(
-                    {"input": question}
-                )
+            return history
+        
+        conversational_rag_chain = RunnableWithMessageHistory(retrieval_chain, get_session_history, input_messages_key='input', history_messages_key='chat_history', output_messages_key='answer')
 
-            st.subheader("Answer")
 
-            st.write(response["answer"])
+
+        user_input = st.text_input("Ask a question about you Pdf: ")
+
+        if user_input:
+            session_history = get_session_history(session_id)
+            response = conversational_rag_chain.invoke({'input':user_input}, config={'configurable': {'session_id': session_id}})
 
             with st.expander("Retrieved Context"):
+                if "context" in response:
+                    for doc in response["context"]:
+                        st.write(doc.page_content)
+                        st.write("----------------")
 
-                for doc in response["context"]:
-                    st.write(doc.page_content)
-                    st.write("---")
+            st.subheader("Assistant Answer: ")
+
+            st.write(response['answer'])
+
+            with st.expander("Chat History"):
+                st.write(f"Stored messages: {len(session_history.messages)} / {MAX_MESSAGES}")
+                st.write(session_history.messages)
 
     else:
         st.info("Please upload a PDF.")
 
 else:
-    st.warning("Please enter your GROQ API Key.")
+    st.warning("Please Enter Your API Key to continue")    
